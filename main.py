988e9058ce4c1b407f145007a728d99fabcea18a
Dmitry Dolgov <me@nyorf.com>
2024-12-15 22:57:49 +0300

    switch b64 return to json

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import io
 import base64
 import cv2
 
+
 app = FastAPI()
 
 @app.get(path="/wallpaper")
@@ -14,7 +15,8 @@ async def getWallpaper(img: bool = False):
         img = cv2.imread('data/wallpaper.jpg')
         jpg_img = cv2.imencode('.jpg', img)
         b64_string = base64.b64encode(jpg_img[1]).decode('utf-8')
-        return b64_string
+        img_json = {'image': b64_string}
+        return img_json
     else:
         image_stream = io.FileIO('data/wallpaper.jpg', mode='r')
         return StreamingResponse(content=image_stream, media_type="image/jpg")
